a79752ec6010609566033afe8e91547ff101ab7c
arrorobbi <vickyrobbi@gmail.com>
2024-09-14 13:39:54 +0700

    feat: embed quill componenct && connecting api

--- src/app/sharingKnowledge/page.tsx
+++ src/app/sharingKnowledge/page.tsx
@@ -1,13 +1,32 @@
-// pages/index.tsx
-import QuillEditor from '../components/QuillEditor';
+"use client";
+import React from "react";
+import QuillEditor from "../components/QuillEditor"; // Adjust the import path as necessary
 
-const HomePage: React.FC = () => {
+interface EnhancedQuillEditorProps {
+  value?: string;
+  readonly?: boolean;
+  onChange?: (content: string) => void;
+  // Add additional props if needed
+}
+
+const EnhancedQuillEditor: React.FC<EnhancedQuillEditorProps> = ({
+  value,
+  onChange,
+  readonly = false,
+}) => {
   return (
-    <div>
-      <h1>Quill.js Editor in Next.js</h1>
-      <QuillEditor readonly:false />
+    <div
+      style={{
+        border: "1px solid #ccc",
+        borderRadius: "4px",
+        padding: "10px",
+        margin: "20px",
+      }}
+    >
+      <h2>Custom Quill Editor</h2>
+      <QuillEditor value={value} onChange={onChange} readonly={readonly} />
     </div>
   );
 };
-  
-export default HomePage;
+
+export default EnhancedQuillEditor;
