e0fd93b8ca53f6c46fb4387435b72ea51f23d695
Josephira <Josephira@clipan.co.id>
2024-09-19 09:48:13 +0700

    fix layout page read

--- src/app/sharingKnowledge/page.tsx
+++ src/app/sharingKnowledge/page.tsx
@@ -39,7 +39,7 @@ export default function SharingKnowledgePage() {
 
   return (
     <div className="p-0 m-0">
-      <div className="pt-0 ml-20 mt-4">
+      <div className="pt-0 ml-40 mt-4">
         <DynamicCom setTab={dataContent} readonly={true} firstData={dataContent[0]}/>
       </div>
     </div>
