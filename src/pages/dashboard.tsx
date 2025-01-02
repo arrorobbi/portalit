56dc14df6742b1e9678009a4e18b2ea95f2d25dd
arrorobbi <vickyrobbi@gmail.com>
2025-01-02 17:16:24 +0700

    set device data || need detail data show

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -8,15 +8,15 @@ import { Activity, CheckCircle } from "lucide-react"; // Replace with your icons
 // Define types for the device and sensor objects
 type Sensor = {
   title: string;
-  value: number;
+  value: number | string;  // Allow value to be either number or string
   unit: string;
   status: string;
   color: string;
-  icon: string; // Use string instead of JSX.Element
+  icon: keyof typeof iconMap;  // Ensure icon matches the keys of the iconMap
 };
 
 type Device = {
-  id: string
+  id: string;
   deviceName: string;
   ipAddress: string;
   dependency: string;
@@ -30,12 +30,12 @@ type DataPageProps = {
 
 // Map icons to JSX components
 const iconMap = {
-    Activity: <Activity className="text-orange-500" />,
-    CheckCircle: <CheckCircle className="text-green-500" />,
-  } as const; // `as const` ensures the keys are treated as literals
-  
-  // Type to restrict icon keys
-  type IconKey = keyof typeof iconMap;
+  Activity: <Activity className="text-orange-500" />,
+  CheckCircle: <CheckCircle className="text-green-500" />,
+} as const;  // `as const` ensures the keys are treated as literals
+
+// Type to restrict icon keys
+type IconKey = keyof typeof iconMap;
 
 const DataPage = ({ devices, token }: DataPageProps) => {
   const router = useRouter();
@@ -70,65 +70,88 @@ const DataPage = ({ devices, token }: DataPageProps) => {
   );
 };
 
-interface  basic {
-    active: string;
-    host: string;
-    hostv6: string;
-    ipversion: string;
-    name: string;
-    parenttags: string[];
-    priority: string;
-    tags: string[];
-  };
+interface basic {
+  active: string;
+  host: string;
+  hostv6: string;
+  ipversion: string;
+  name: string;
+  parenttags: string[];
+  priority: string;
+  tags: string[];
+}
 
-interface  parent {
-    id: string;
-    name: string;
-    type: string;
-    href: string;
-  };
+interface parent {
+  id: string;
+  name: string;
+  type: string;
+  href: string;
+}
 
 interface AllDevice {
-    basic: basic
-    href: string;
-    id: string;
-    kind: string;
-    kind_variant: string;
-    name: string;
-    parent: parent
-    status: string;
-    type: string;
-  }
+  basic: basic;
+  href: string;
+  id: string;
+  kind: string;
+  kind_variant: string;
+  name: string;
+  parent: parent;
+  status: string;
+  type: string;
+}
 
-  
-  export const getServerSideProps = async () => {
-    const payload = {
-      username: "prtgadmin",
-      password: "prtgCC1x",
-    };
-  
-    let devices: Device[] = [];
-    let token = "";
-  
-    try {
-      const login = await fetch(
-        `${process.env.NEXT_PUBLIC_API_PRTG_URL}/api/v2/session`,
+interface parsedSensor {
+  id: string;
+  name: string;
+  status: string;
+  // Add other properties here based on your data structure
+}
+
+export const getServerSideProps = async () => {
+  const payload = {
+    username: "prtgadmin",
+    password: "prtgCC1x",
+  };
+
+  let devices: Device[] = [];
+  let token = "";
+
+  try {
+    const login = await fetch(
+      `${process.env.NEXT_PUBLIC_API_PRTG_URL}/api/v2/session`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      }
+    );
+
+    if (login.ok) {
+      const loginData = await login.json();
+      token = loginData.token;
+      console.log(loginData);
+
+      const getDevice = await fetch(
+        `${process.env.NEXT_PUBLIC_API_PRTG_URL}/api/v2/experimental/devices?offset=0&limit=100`,
         {
-          method: "POST",
+          method: "GET",
           headers: {
             "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`,
           },
-          body: JSON.stringify(payload),
         }
       );
-  
-      if (login.ok) {
-        const loginData = await login.json();
-        token = loginData.token;
-        console.log(loginData);
-  
-        const getDevice = await fetch(
-          `${process.env.NEXT_PUBLIC_API_PRTG_URL}/api/v2/experimental/devices?offset=0&limit=100`,
+
+      const deviceData = await getDevice.json();
+      console.log("DATAA: ", deviceData[0].basic["host"]);
+
+      // Use Promise.all to wait for all async operations to complete
+      const devicePromises = deviceData.map(async (device: AllDevice) => {
+        // Fetch details for each device asynchronously
+        const getDetails = await fetch(
+          `${process.env.NEXT_PUBLIC_API_PRTG_URL}/api/v2/sensors?filter=ancestors contains "${device.id}"&limit=0&include_all_channels=true`,
           {
             method: "GET",
             headers: {
@@ -137,69 +160,55 @@ interface AllDevice {
             },
           }
         );
-  
-        const deviceData = await getDevice.json();
-        console.log("DATAA: ", deviceData[0].basic['host']);
-  
-        // Use Promise.all to wait for all async operations to complete
-        const devicePromises = deviceData.map(async (device: AllDevice) => {
-          // Fetch details for each device asynchronously
-          const getDetails = await fetch(
-            `${process.env.NEXT_PUBLIC_API_PRTG_URL}/api/v2/sensors?filter=ancestors contains "${device.id}" &limit=0&include_all_channels=true`,
+
+        const details = await getDetails.json();
+        const pingDisplayVolume = details.find((sensor: any) => sensor.name === "Ping")?.channels[0]?.last_measurement?.display_volume;
+        const sflowDisplayVolume = details.find((sensor: any) => sensor.name === "sFlow")?.channels[0]?.last_measurement?.display_volume;
+
+        // Return the device data after async fetch completes
+        console.log("Ping display_volume:", pingDisplayVolume); // null
+        console.log("sFlow display_volume:", sflowDisplayVolume); // 3.78125
+        return {
+          id: device.id,
+          deviceName: device.name,
+          ipAddress: device.basic["host"] || "invalid",
+          dependency: device.parent["name"] || "invalid",
+          sensors: [
             {
-              method: "GET",
-              headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${token}`,
-              },
-            }
-          );
-  
-          const details = await getDetails.json();
-        //   console.log("Details: ", details[1]['channels'][0]['last_measurement']['display_value']);
-        //   const sflowValue = details[1]['channels'][0]['last_measurement']['display_value'] || 0
-          // Return the device data after async fetch completes
-          return {
-            id: device.id,
-            deviceName: device.name,
-            ipAddress: device.basic['host'] || 'invalid',
-            dependency: device.parent['name'] || 'invalid',
-            sensors: [
-              {
-                title: "sFlow",
-                value: 0.3,
-                unit: "Mbit/s",
-                status: "get data",
-                color: "#F59E0B",
-                icon: "Activity", // Store as string
-              },
-              {
-                title: details[0]['name'],
-                value: 19,
-                unit: "msec",
-                status: "get data",
-                color: "#84CC16",
-                icon: "CheckCircle", // Store as string
-              },
-            ],
-          };
-        });
-  
-        // Wait for all async operations to finish before proceeding
-        devices = await Promise.all(devicePromises);
-      } else {
-        console.error("Failed to fetch session data:", login.statusText);
-      }
-    } catch (error) {
-      console.error("Error during API request:", error);
+              title: "sFlow",
+              value: sflowDisplayVolume || "no data",
+              unit: "Mbit/s",
+              status: "get data",
+              color: "#F59E0B",
+              icon: "Activity", // Store as string
+            },
+            {
+              title: details[0]["name"],
+              value: pingDisplayVolume || "no data",
+              unit: "msec",
+              status: "get data",
+              color: "#84CC16",
+              icon: "CheckCircle", // Store as string
+            },
+          ],
+        };
+      });
+
+      // Wait for all async operations to finish before proceeding
+      devices = await Promise.all(devicePromises);
+    } else {
+      console.error("Failed to fetch session data:", login.statusText);
     }
-  
-    return {
-      props: {
-        devices,
-        token,
-      },
-    };
+  } catch (error) {
+    console.error("Error during API request:", error);
+  }
+
+  return {
+    props: {
+      devices,
+      token,
+    },
   };
-  
+};
+
 export default DataPage;
